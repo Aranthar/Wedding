2e64272940eaa11011574566de854959088f6ac5
Aranthar <dvkolomiets@gmail.com>
2021-08-03 00:17:09 +0500

    Countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  AOS.init();
+  AOS.init()
   
   $('body').on('click', '[href*="#"]', event => {
     $('html,body').stop().animate({ 
@@ -11,12 +11,46 @@ $(document).ready(function () {
   })
 
   AOS.init({
-
     disable: 'phone',
     offset: 120,
     duration: 800,
     once: false,
+  })
 
-  });
+  var countdownSeconds = 1056605
 
-});
+  let timerID = setInterval(() => {
+    countdownSeconds--
+
+    if (countdownSeconds == 0) {
+      clearInterval(timerID)
+    }
+
+    countdownDraw(countdownSeconds)
+  }, 1000)
+
+  const countdownDraw = (initialSeconds) => {
+    let days = Math.floor(initialSeconds / 86400)
+    let hours = Math.floor((initialSeconds - (days * 86400)) / 3600)
+    let minutes = Math.floor((initialSeconds - (days * 86400) - (hours * 3600)) / 60)
+    let seconds = Math.floor((initialSeconds - (days * 86400) - (hours * 3600) - (minutes * 60)))
+
+    if (seconds < 10) {
+      seconds = '0' + seconds
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes
+    }
+
+    if (hours < 10) {
+      hours = '0' + hours
+    }
+
+    $('#countdownDays').text(days)
+    $('#countdownHours').text(hours)
+    $('#countdownMinutes').text(minutes)
+    $('#countdownSeconds').text(seconds)
+  }
+
+})
